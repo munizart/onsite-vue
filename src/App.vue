4752b9b643ee8230fa5ebedad340b0b6dfca8e25
munizart <munizart_dev@hotmail.com>
2018-05-07 19:28:35 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Widget title="Teste" strategy="EndedUpBuying"/>
+  </div>
+</template>
+
+<script>
+import Widget from './components/Widget'
+
+export default {
+  name: 'App',
+  components: {
+    Widget
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Widget.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <Carousel>
+      <div slot-scope="{ prop1 }">
+        {{ prop1 }}
+      </div>
+    </Carousel>
+  </div>
+</template>
+
+<script>
+import Carousel from './Carousel'
+
+export default {
+  components: {
+    Carousel
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    strategy: {
+      type: String,
+      validator: (strategyName) => strategyName && /(?:Similar)|(?:FrequentlyBoughtTogether)|(?:EndedUpBuying)|(?:BestSellers)|(?:MostVisited)|(?:Trending)/.test(strategyName),
+      required: true
+    },
+    productFormat: {
+      type: String,
+      validator: (productFormatType) => /comp(?:act)|(?:lete)/.test(productFormatType),
+      default: 'compact'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
